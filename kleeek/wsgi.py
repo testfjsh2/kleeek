1ce4163b088065cda3630be35c05bbe902835b57
fjshadows <fjshadows@BHC.(none)>
2014-01-14 22:39:42 +0400

    edit admins

--- kleeek/wsgi.py
+++ kleeek/wsgi.py
@@ -7,8 +7,15 @@ For more information on this file, see
 https://docs.djangoproject.com/en/1.6/howto/deployment/wsgi/
 """
 
-import os
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kleeek.settings")
+import os, sys, site
 
-from django.core.wsgi import get_wsgi_application
-application = get_wsgi_application()
+sys.path.insert(0, os.path.dirname(__file__))
+
+site.addsitedir('/usr/local/lib/python2.7/dist-packages')
+os.environ['DJANGO_SETTINGS_MODULE'] = 'kleeek.settings'
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "kleeek.settings")
+
+from django.core.handlers.wsgi import WSGIHandler
+application = WSGIHandler()
+# from django.core.wsgi import get_wsgi_application
+# application = get_wsgi_application()
